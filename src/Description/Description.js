2b066b9eb1b32af5eaeeb495b6c46418420dfc80
Ruby Rinken <rubyrinken@gmail.com>
2020-07-20 17:01:25 -0600

    Add neat way of conveying information in description component

--- src/Description/Description.js
+++ src/Description/Description.js
@@ -1,12 +1,30 @@
 import React from "react";
 import "./Description.css";
+import { usePlayerState } from "../playerState";
 
 const Description = () => {
-  return (
-    <p className="Description">
-      You are in a grassy field. Your health is running low.
-    </p>
-  );
+  const player = usePlayerState();
+  // starting string, can be changed according to player's last location
+  let description = "You are in a grassy field. ";
+
+  // array of arrays, first element of each subarray is a query into the player's stats,
+  // second element is a bit of info to add to the player's description
+  // ex: details[0] takes in the player's hp and maxhp and if the hp is less then half
+  // of the maxhp, gives back a "you are hurt" message.
+  const details = [
+    [({ hp, maxhp }) => hp < maxhp / 2, "You are hurt. "],
+    [({ attack }) => attack < 3, "You feel weak. "],
+    [({ defense }) => defense < 3, "You feel defenseless. "],
+    [({ name }) => !name, "You feel a loss of identity. "],
+  ];
+
+  // if the query for each element in the details array returns true,
+  // add the relevant info to the description string. otherwise don't
+  description += details
+    .map(([query, info]) => (query(player) ? info : ""))
+    .join("");
+
+  return <p className="Description">{description}</p>;
 };
 
 export default Description;
