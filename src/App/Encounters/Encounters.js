19294fd2f47701087cc9d76be2c20e1989235e03
Trond Makonese <trondation@gmail.com>
2020-07-22 08:58:28 -0600

    Add div around sections of the page for styling purposes. Maybe use grid.

--- src/App/Encounters/Encounters.js
+++ src/App/Encounters/Encounters.js
@@ -5,6 +5,8 @@ import { usePlayerState } from "../playerState";
 
 const Encounters = () => {
 //WIll need conditionally render this component based on wether the encounter is a battle or a friendly npc
+//DO we need the props to be able to use player actions?
+//This is a basic layout, things can change
   const player = usePlayerState();
   
   return (
@@ -16,8 +18,8 @@ const Encounters = () => {
             <aside className='npc-text-box'>
                 <p>Get in my belly, or i'll eat your brain</p>
             </aside>
-            <div>
         </div>
+        <div className='action-button-div'>
             <button>
                 Talk About Adventure
             </button>
