f9277652fcf95d0d137707e99178d2277833ecba
Trond Makonese <trondation@gmail.com>
2020-07-22 09:32:55 -0600

    Fix route for encounters.

--- src/Encounters/Encounters.js
+++ src/Encounters/Encounters.js
@@ -1,34 +1,19 @@
 import React from "react";
-import "./UserStats.css";
-import { usePlayerState } from "../playerState";
+// import { usePlayerState } from "../playerState";
 
 
 const Encounters = () => {
 //WIll need conditionally render this component based on wether the encounter is a battle or a friendly npc
 //DO we need the props to be able to use player actions?
 //This is a basic layout, things can change
-  const player = usePlayerState();
+//   const player = usePlayerState();
   
   return (
     <section className='ecounters-section'>
         <div className='main-container'>
             <div className='encounter-div'>
-                <p>Encounters</p>
+                <p>Encounter Made It</p>
             </div>
-            <aside className='npc-text-box'>
-                <p>Get in my belly, or i'll eat your brain</p>
-            </aside>
-        </div>
-        <div className='action-button-div'>
-            <button>
-                Talk About Adventure
-            </button>
-            <button>
-                Talk About Cats
-            </button>
-            <button>
-                Leave
-            </button>
         </div>
     </section>
   );
