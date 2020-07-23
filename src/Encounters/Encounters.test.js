045630540324deb9e33d5cb62fc0784e025f4780
Trond Makonese <trondation@gmail.com>
2020-07-23 09:58:56 -0600

    initial test for rendering the encounters component passes.

---
+++ src/Encounters/Encounters.test.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Encounters from "./Encounters";
+import { MemoryRouter } from "react-router-dom";
+
+describe("Encounters", () => {
+  it("should render without crashing", () => {
+    render(
+      <MemoryRouter>
+        <Encounters />
+      </MemoryRouter>
+    );
+  });
+
+//   it("should show the user some possible actions`", () => {
+//     const { getAllByText } = render(
+//       <MemoryRouter>
+//         <Actions />
+//       </MemoryRouter>
+//     );
+//     expect(getAllByText("Go to", { exact: false }).length).toBeGreaterThan(0);
+//   });
+});
