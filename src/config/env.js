b1b716c3ccd839366a7a15ff765f03d19960be3b
rubyr <rubyrinken@gmail.com>
2020-07-25 16:31:15 -0600

    Remove old env files

--- src/config/env.js
+++
@@ -1,7 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config();
-
-export const APP_URL = process.env.APP_URL;
-export const API_URL = process.env.API_URL;
-export const NODE_ENV = process.env.NODE_ENV;
-export const PORT = process.env.PORT || 3000;
