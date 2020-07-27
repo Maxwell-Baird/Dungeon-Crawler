70bc3f9ba6eb27af762297066022bedc914287fb
Trond Makonese <trondation@gmail.com>
2020-07-27 09:57:47 -0600

    Add test branch.

--- src/Encounters/Encounters.test.js
+++ src/Encounters/Encounters.test.js
@@ -3,13 +3,20 @@ import { render } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import Encounters from "./Encounters";
 import { MemoryRouter } from "react-router-dom";
+import { PlayerContextProvider } from "./playerState";
+import { EncounterContextProvider } from "./encounterState";
 
 describe("Encounters", () => {
   it("should render without crashing", () => {
     render(
-      <MemoryRouter>
-        <Encounters />
-      </MemoryRouter>
+        <MemoryRouter>
+        <PlayerContextProvider>
+        <EncounterContextProvider>
+                <Encounters />
+        </EncounterContextProvider>
+        </PlayerContextProvider>
+        </MemoryRouter>
+
     );
   });
 
