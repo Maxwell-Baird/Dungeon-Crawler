27c7479b438def666c13cba7a02d313369e565af
Ruby Rinken <rubyrinken@gmail.com>
2020-07-28 17:48:03 -0600

    Remove dependencies on env

--- src/ApiCalls.js
+++ src/ApiCalls.js
@@ -1,3 +1,8 @@
-export const getNpcs = async () => await (
-  await fetch(process.env.REACT_APP_API_URL + "/npcs")
-).json();
+export const getNpcs = async () =>
+  await (
+    await fetch(
+      (process.env.NODE_ENV === "production"
+        ? "https://whispering-temple-46123.herokuapp.com/api/v1"
+        : "http://localhost:8000/api/v1") + "/npcs"
+    )
+  ).json();
