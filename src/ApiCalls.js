02c5993a37065a7d5605dfb37c749d00888a8a15
Ruby Rinken <rubyrinken@gmail.com>
2020-07-28 17:53:03 -0600

    Fix to work with BE

--- src/ApiCalls.js
+++ src/ApiCalls.js
@@ -3,6 +3,6 @@ export const getNpcs = async () =>
     await fetch(
       (process.env.NODE_ENV === "production"
         ? "https://whispering-temple-46123.herokuapp.com/api/v1"
-        : "http://localhost:8000/api/v1") + "/npcs"
+        : "http://localhost:8000/api/v1") + "/npcs?format=json"
     )
   ).json();
