36eb8e7148e5f2f7db469ae75913f5016e635311
Justin Corbin <jcorbin2290@gmail.com>
2020-07-28 11:21:45 -0600

    Created test coverage for FightAction

--- src/Fight/FightActions/FightActions.test.js
+++ src/Fight/FightActions/FightActions.test.js
@@ -7,25 +7,26 @@ import { PlayerContextProvider } from "../../playerState";
 import { EncounterContextProvider } from "../../encounterState";
 import { BrowserRouter as Router } from "react-router-dom";
 import { getNpcs } from '../../ApiCalls';
+import { StateContext as NPCcontext, DispatchContext as NPCdispatchContext } from '../../encounterState'
 
-jest.mock("../../ApiCalls", () => ({
-  getNpcs: () => ([{
-  location: 'Forest',
-  name: 'Goblin',
-  health: 5,
-  attack: 3,
-  defense: 2,
-  options: [{
-    name: "Fight",
-    type: "aggressive"
-  },
-  {
-    name: "Talk",
-    type: "passive"
-  }
-  ],
-  }])
-  }))
+// jest.mock("../../ApiCalls", () => ({
+//   getNpcs: () => ([{
+//   location: 'Forest',
+//   name: 'Goblin',
+//   health: 5,
+//   attack: 3,
+//   defense: 2,
+//   options: [{
+//     name: "Fight",
+//     type: "aggressive"
+//   },
+//   {
+//     name: "Talk",
+//     type: "passive"
+//   }
+//   ],
+//   }])
+//   }))
 
 describe("FightActions", () => {
   it("should render", () => {
@@ -39,15 +40,33 @@ describe("FightActions", () => {
   });
 
   it("name this test", async () => {
-    
+
     const { getByText, getByPlaceholderText } = render(
       <Router>
-        <PlayerContextProvider>
-          <EncounterContextProvider>
-            <FightActions/>
-          </EncounterContextProvider>
+      <PlayerContextProvider>
+        <NPCdispatchContext.Provider value={{attack: () => {}}}>
+          <NPCcontext.Provider value={{isLoading: false, npc: {
+            location: 'Forest',
+            name: 'Goblin',
+            health: 5,
+            attack: 3,
+            defense: 2,
+            options: [{
+              name: "Fight",
+              type: "aggressive"
+            },
+            {
+              name: "Talk",
+              type: "passive"
+              }
+              ]
+              }}}>
+              <FightActions/>
+            </NPCcontext.Provider>
+          </NPCdispatchContext.Provider>
         </PlayerContextProvider>
       </Router>)
+
     const awaitedText = await waitForElement(() => getByText("Attack"))
     expect(awaitedText).toBeInTheDocument();
   });
