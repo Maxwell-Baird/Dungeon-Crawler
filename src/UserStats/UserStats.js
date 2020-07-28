8365d8b6db2f49896a24fc97d5bfb8c89e4e3d26
Ruby Rinken <rubyrinken@gmail.com>
2020-07-28 14:25:12 -0600

    Allow user to clear data & add button for doing so

--- src/UserStats/UserStats.js
+++ src/UserStats/UserStats.js
@@ -1,9 +1,17 @@
 import React from "react";
-import { usePlayerState } from "../playerState";
+import { usePlayerState, usePlayerDispatch } from "../playerState";
 import "./UserStats.css";
 
 const UserStats = () => {
   const player = usePlayerState();
+  const { clearLSData } = usePlayerDispatch();
+
+  const showAlert = () => {
+    window.confirm(
+      "WARNING: This will clear ALL your save data and restart your game! Are you sure you wish to continue?"
+    ) && clearLSData();
+  };
+
   return (
     <div className="User-Stats">
       <span>Name: {player.name}</span>
@@ -12,6 +20,9 @@ const UserStats = () => {
       </span>
       <span>Atk: {player.attack}</span>
       <span>Def: {player.defense}</span>
+      <button className="warning" onClick={showAlert}>
+        Clear Save & Quit
+      </button>
     </div>
   );
 };
