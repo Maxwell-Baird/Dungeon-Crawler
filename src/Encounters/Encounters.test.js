693c5c68e73fc467828ef5c3a6b1451ff1cea40c
Ruby Rinken <rubyrinken@gmail.com>
2020-07-29 15:02:27 -0600

    Get Encounter test working

--- src/Encounters/Encounters.test.js
+++ src/Encounters/Encounters.test.js
@@ -1,71 +1,49 @@
 import React from "react";
-import { render } from "@testing-library/react";
 import Encounters from "./Encounters";
 import "@testing-library/jest-dom/extend-expect";
-import { PlayerContextProvider } from "../playerState";
-import { EncounterContextProvider } from "../encounterState";
+import { wait } from "@testing-library/dom";
+import TestWrapper from "../TestWrapper/TestWrapper";
 import { getNpcs } from "../ApiCalls";
 jest.mock("../ApiCalls");
+//@ts-ignore
 getNpcs.mockImplementation(() =>
   Promise.resolve([
     {
+      id: 3,
+      name: "Bear",
+      attack: 8,
+      defense: 5,
+      health: 12,
       location: "Forest",
-      name: "Goblin",
-      health: 5,
-      attack: 3,
-      defense: 2,
+      description:
+        "You wander through the forest until you come across a bear, sleeping near a tree. You realize it is a bear a moment too late, and it awakes at your presence. It seems tired and annoyed that you woke it up. ",
     },
   ])
 );
 
 describe("Encounters", () => {
-  it("should render", () => {
-    render(
-      <PlayerContextProvider>
-        <EncounterContextProvider>
-          <Encounters location="forest" />
-        </EncounterContextProvider>
-      </PlayerContextProvider>
-    );
+  it("should render", async () => {
+    TestWrapper(<Encounters location="forest" />);
+    await wait();
   });
-  it("should display npcs name", () => {
-    const { getByText } = render(
-      <PlayerContextProvider>
-        <EncounterContextProvider>
-          <Encounters location={"forest"} />
-        </EncounterContextProvider>
-      </PlayerContextProvider>
-    );
-    expect(getByText("Goblin")).toBeInTheDocument();
+  it("should display npcs name", async () => {
+    const { getByText } = TestWrapper(<Encounters location="forest" />);
+    await wait();
+    expect(getByText("Bear")).toBeInTheDocument();
   });
-  it("should display npcs defense", () => {
-    const { getByText } = render(
-      <PlayerContextProvider>
-        <EncounterContextProvider>
-          <Encounters location={"forest"} />
-        </EncounterContextProvider>
-      </PlayerContextProvider>
-    );
-    expect(getByText("DEF: 2")).toBeInTheDocument();
+  it("should display npcs defense", async () => {
+    const { getByText } = TestWrapper(<Encounters location="forest" />);
+    await wait();
+    expect(getByText("DEF: 5")).toBeInTheDocument();
   });
-  it("should display npcs health", () => {
-    const { getByText } = render(
-      <PlayerContextProvider>
-        <EncounterContextProvider>
-          <Encounters location={"forest"} />
-        </EncounterContextProvider>
-      </PlayerContextProvider>
-    );
-    expect(getByText("5 / 5 hp")).toBeInTheDocument();
+  it("should display npcs health", async () => {
+    const { getByText } = TestWrapper(<Encounters location="forest" />);
+    await wait();
+    expect(getByText("12 / 12 hp")).toBeInTheDocument();
   });
-  it("should display npcs attack", () => {
-    const { getByText } = render(
-      <PlayerContextProvider>
-        <EncounterContextProvider>
-          <Encounters location={"forest"} />
-        </EncounterContextProvider>
-      </PlayerContextProvider>
-    );
-    expect(getByText("ATK: 3")).toBeInTheDocument();
+  it("should display npcs attack", async () => {
+    const { getByText } = TestWrapper(<Encounters location="forest" />);
+    await wait();
+    expect(getByText("ATK: 8")).toBeInTheDocument();
   });
 });
